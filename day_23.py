6e56ba51e545f623c3359ab140ce5fc8e2ce5f6c
Juan Manuel Consoli <jconsoli@evertz.com>
2024-12-23 19:28:54 +0000

    day 23 part 2

--- day_23.py
+++ day_23.py
@@ -11,10 +11,29 @@ for x, y in edges:
 
 sets: Set[str] = set()
 
-for x in conns:
-    for y in conns[x]:
-        for z in conns[y]:
-            if x != z and x in conns[z]:
-                sets.add(tuple(sorted([x, y, z])))
+# for x in conns:
+#     for y in conns[x]:
+#         for z in conns[y]:
+#             if x != z and x in conns[z]:
+#                 sets.add(tuple(sorted([x, y, z])))
 
-print(len([s for s in sets if any(cn.startswith("t") for cn in s)]))
+# print(len([s for s in sets if any(cn.startswith("t") for cn in s)]))
+
+
+def search(node, req):
+    key = tuple(sorted(req))
+    if key in sets:
+        return
+    sets.add(key)
+    for neighbor in conns[node]:
+        if neighbor in req:
+            continue
+        if not all(neighbor in conns[query] for query in req):
+            continue
+        search(neighbor, {*req, neighbor})
+
+
+for x in conns:
+    search(x, {x})
+
+print(",".join(sorted(max(sets, key=len))))
